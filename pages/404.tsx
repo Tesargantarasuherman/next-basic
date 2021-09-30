0792b204a84d46fe2bc479cbfe3decd691275ded
Tesar gantara suherman <tgantara@gmail.com>
2021-09-30 15:37:15 +0700

    update

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Custom404 = () => {
+    return (
+        <div>
+            <h1 className="title-not-found">Ooopppss......</h1>
+            <h1 className="title-not-found">Halaman Tidak Ditemukan</h1>
+        </div>
+    );
+}
+
+export default Custom404;
