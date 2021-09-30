dd85370220b67f6a8c299291e003d457c9156818
Tesar gantara suherman <tgantara@gmail.com>
2021-09-30 21:24:21 +0700

    router dinamis

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,12 @@
+import { useRouter } from "next/dist/client/router";
 import Layout from "../../components/Layout";
-
+// user/{id}
 const userDetail = () => {
+    const router = useRouter();
+    const {id} = router.query;
     return (
         <Layout pageTitle="User Detail">
-            User Details
+            User Details {id}
         </Layout>
     );
 }
