27dc11390e5573914bb81e9a17da68129d2571cf
Tesar gantara suherman <tgantara@gmail.com>
2021-09-30 22:27:31 +0700

    props dinamis

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,11 +1,32 @@
 import Layout from '../../components/Layout';
 
-const indexUsers = () => {
+// tipe variabel props
+interface UserProps{
+    dataUser: Array<any>;
+}
+// 
+const indexUsers = (props:UserProps) => {
+    const {dataUser} =  props;
+    console.log(dataUser)
     return (
         <Layout pageTitle="Users">
-            index user
+            {dataUser.map((user:any)=>{
+                return(
+                    <>
+                    <p >{user.name}</p>
+                    </>
+                )
+            })}
         </Layout>
     );
 }
-
+export async function getStaticProps(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUser = await res.json();
+    return{
+        props: {
+            dataUser
+        }
+    }
+}
 export default indexUsers ;
