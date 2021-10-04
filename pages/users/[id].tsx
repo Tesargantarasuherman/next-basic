716b8cba3d8234b99b1c9e23975391b21a351dfd
unknown <tgantara@gmail.com>
2021-10-04 09:15:58 +0700

    update

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -2,13 +2,23 @@ import { useRouter } from "next/dist/client/router";
 import Layout from "../../components/Layout";
 // user/{id}
 const userDetail = () => {
-    const router = useRouter();
-    const {id} = router.query;
-    return (
-        <Layout pageTitle="User Detail">
-            User Details {id}
-        </Layout>
-    );
-}
+  const router = useRouter();
+  const { id } = router.query;
+  return <Layout pageTitle="User Detail">User Details {id}</Layout>;
+};
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUser = await res.json();
 
+  const paths = dataUser.map((user: any) => ({
+    params: {
+      id: user.id,
+    },
+  }));
+  return {
+    paths: {
+      dataUser,
+    },
+  };
+}
 export default userDetail;
