0af68886c6fd304890d5f178e58f1c36a6d80aa1
unknown <tgantara@gmail.com>
2021-10-04 10:04:44 +0700

    update

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,24 +1,44 @@
 import { useRouter } from "next/dist/client/router";
 import Layout from "../../components/Layout";
-// user/{id}
-const userDetail = () => {
+// users/{id}
+
+interface UserDetailProps{
+  user:object;
+}
+const userDetail = (props:any) => {
   const router = useRouter();
   const { id } = router.query;
-  return <Layout pageTitle="User Detail">User Details {id}</Layout>;
+  const {user} = props;
+  return (
+  <Layout pageTitle="User Detail">
+      <p>{user.name}</p>
+      <p>{user.phone}</p>
+  </Layout>
+  )
 };
+
 export async function getStaticPaths() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const dataUser = await res.json();
 
   const paths = dataUser.map((user: any) => ({
     params: {
-      id: user.id,
+      id:`${user.id}`,
     },
   }));
   return {
-    paths: {
-      dataUser,
-    },
+    paths,
+    fallback:true
   };
+}
+export async function getStaticProps(context:any){
+  const {id} = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+  return{
+    props:{
+      user,
+    }
+  }
 }
 export default userDetail;
