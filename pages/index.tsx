36a874656750e21cdec239c46586c0d696b7e6d3
unknown <tgantara@gmail.com>
2021-10-05 21:27:48 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import Layout from '../components/Layout';
-
-const Home = () => {
+import Cookies from "js-cookie";
+import styles from '../styles/Home.module.css'
+export default function Home (){
   return (
     <>
       <Layout pageTitle="Home Page">
-      <h1>Tesar Gantara</h1>
+      <div className={styles.container}>
+      <button type="button" onClick={()=>{
+        Cookies.set("token","ABCDEF",{expires:1/24})
+      }}>Login</button>
+      <button type="button" onClick={()=>{}}>Login</button>
+      </div>
       </Layout >
     </>
   )
 }
 
-export default Home 
+interface parameter{
+  req:any;
+  res:any
+}
+
+export function getServerSideProps(props:parameter){
+  return{
+    props:{}
+  }
+}
