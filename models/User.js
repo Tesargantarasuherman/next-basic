78d91bda769977b9684425fb2bb82ef343880e70
unknown <tgantara@gmail.com>
2021-10-10 01:15:54 +0700

    update

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, 'Please add a title'],
+        required: [true, 'Please add a email'],
         unique: true,
         trim: true,    
     },
