d85ab8372604dbd59aa90545c868c67bea9c738c
unknown <tgantara@gmail.com>
2021-10-09 15:12:29 +0700

    update

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -14,17 +14,17 @@ const indexBlog = (props: BlogProps) => {
     return (
         <Layout pageTitle="Blog">
             <Grid divided='vertically' padded className={styles.card_margin_top}>
-                <Grid.Row columns={3}  >
+                <Grid.Row columns={4}  >
                     {dataBlog.map((blog: any) => {
                         return (
                             <Grid.Column >
                                 <Card key={blog._id}>
                                     <Image src='https://images.unsplash.com/photo-1633114127408-af671c774b39?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80' wrapped ui={false} />
                                     <Card.Content>
-                                        <Card.Header>Daniel</Card.Header>
+                                        <Card.Header>{blog.title}</Card.Header>
                                         <Card.Meta>Joined in 2016</Card.Meta>
                                         <Card.Description>
-                                            Daniel is a comedian living in Nashville.
+                                            {blog.description}
                                         </Card.Description>
                                     </Card.Content>
                                     <Card.Content extra>
