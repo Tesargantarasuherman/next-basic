38be154089f5c421f8826d18820fdc237dfe7bb4
unknown <tgantara@gmail.com>
2021-10-14 19:51:17 +0700

    update logout

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,33 @@
 import Link from 'next/link';
 import styles from './Header.module.css';
+import { useRouter } from 'next/router';
+import { Button} from 'semantic-ui-react'
 
 const Header = () => {
+  const router = useRouter();
+
+  const submitLogout = async () => {
+    let token = localStorage.getItem('token');
+    try {
+      const res = await fetch('http://localhost:3000/api/user/logout', {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ token:token})
+      },)
+      router.push("/")  
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div>
       <header className={styles.container}>
         <ul className={styles.list}>
-        <li className={styles.item}>
+          <li className={styles.item}>
             <Link href="/">Home</Link>
           </li>
           <li className={styles.item}>
@@ -14,6 +35,9 @@ const Header = () => {
           </li>
           <li className={styles.item}>
             <Link href="/users">User</Link>
+          </li>
+          <li className={styles.item}>
+            <Button onClick={submitLogout}>Logout</Button>
           </li>
         </ul>
       </header>
